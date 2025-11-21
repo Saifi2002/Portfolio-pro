1d7e1716422b0683b6fe69c6679454dcf23a5b8e
Saif Hussain <hsaifhussain810@gmail.com>
2025-11-21 19:35:43 +0500

    Second Coomit

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="site-footer mx-auto py-12 bg-gray-900 text-white text-center">
+<footer class="site-footer py-12 bg-gray-900 text-white text-center">
     <div class="container mx-auto px-6">
         <p><?php echo get_theme_mod('footer_copyright', '&copy; ' . date('Y') . ' ' . get_bloginfo('name') . '. All rights reserved.'); ?></p>
     </div>
